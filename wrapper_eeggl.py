5c0fe1dabba9ffe98b4795b37c0ed74ed54ecaed
Diego <lloverasdiego@gmail.com>
2024-03-01 11:50:14 -0300

    contains all 3 set of images for a specific event. Diego is using this for EEGGL project

---
+++ wrapper_eeggl.py
@@ -0,0 +1,125 @@
+#wrapper of event from 20110215
+
+#
+
+def wrapper_20110215(frame):
+
+    if frame == 1:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_012400_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_013900_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_012400_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_013900_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361973.fts'
+        lasco_1 = dir_lasco + '25361974.fts'
+    if frame == 2:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_013900_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_015400_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_013900_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_015400_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361975.fts'
+        lasco_1 = dir_lasco + '25361976.fts'
+    if frame == 3:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_015400_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_022400_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_015400_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_022400_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361977.fts'
+        lasco_1 = dir_lasco + '25361978.fts'
+    if frame == 4:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_022400_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_023900_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_022400_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_023900_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361978.fts'
+        lasco_1 = dir_lasco + '25361979.fts'
+
+    if frame == 5:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_023900_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_025400_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_023900_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_025400_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361979.fts'
+        lasco_1 = dir_lasco + '25361980.fts'
+
+    if frame == 6:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_025400_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_032400_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_025400_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_032400_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361984.fts'
+        lasco_1 = dir_lasco + '25361985.fts'
+
+    if frame == 7:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_032400_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_033900_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_032400_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_033900_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361985.fts'
+        lasco_1 = dir_lasco + '25361986.fts'
+
+    if frame == 8:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_033900_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_035400_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_033900_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_035400_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361987.fts'
+        lasco_1 = dir_lasco + '25361988.fts'
+
+    if frame == 9:
+        dir_cor2a = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2A/lvl1/'
+        cor2a_0 = dir_cor2a + '20110215_035400_14c2A.fts'
+        cor2a_1 = dir_cor2a + '20110215_042400_14c2A.fts'
+
+        dir_cor2b = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/Cor2B/lvl1/'
+        cor2b_0 = dir_cor2b + '20110215_035400_14c2B.fts'
+        cor2b_1 = dir_cor2b + '20110215_042400_14c2B.fts'
+
+        dir_lasco = '/data_local/GCS/gcs/Imagenes/NRL/2011-02-15/C2/lvl1/'
+        lasco_0 = dir_lasco + '25361989.fts'
+        lasco_1 = dir_lasco + '25361990.fts'
+
+    base_images = [cor2a_0, cor2b_0, lasco_0]
+    cme_images  = [cor2a_1, cor2b_1, lasco_1]
+    return base_images, cme_images
+
+
