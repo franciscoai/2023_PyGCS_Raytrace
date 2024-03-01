1f80813912cb8dae930ad4f2bd9ecb712c804326
Diego <lloverasdiego@gmail.com>
2024-03-01 11:48:16 -0300

    main rutine for Diego projects with EEGGL

--- gcs_test_diego.py
+++ gcs_test_diego.py
@@ -10,19 +10,20 @@ import pyGCS
 import get_event_gcs
 from astropy.io import fits
 import numpy as np
+from wrapper_eeggl import *
+
 
 if __name__ == "__main__":
+    frame = 8
+    instrument = "cor2a"
+    fecha = "2011-05-15"
+    save_name = fecha+instrument+"_time_"+str(frame)+".txt"
+    base_images, cme_images = wrapper_20110215(frame)
 
-    lasco_0 = '/media/gehme/gehme/data/soho/lasco/level_1/c2/20110605/25374840.fts'
-    cor2a_0 = '/media/gehme/gehme/data/stereo/secchi/L1/a/img/cor2/20110605/20110605_073900_14c2A.fts'
-    cor2b_0 = '/media/gehme/gehme/data/stereo/secchi/L1/b/img/cor2/20110605/20110605_073900_14c2B.fts'
-    lasco_1 = '/media/gehme/gehme/data/soho/lasco/level_1/c2/20110605/25374842.fts'
-    cor2a_1 = '/media/gehme/gehme/data/stereo/secchi/L1/a/img/cor2/20110605/20110605_075400_14c2A.fts'
-    cor2b_1 = '/media/gehme/gehme/data/stereo/secchi/L1/b/img/cor2/20110605/20110605_075400_14c2B.fts'
-
-    fnameA0, fnameB0, fnameL0 = lasco_0, cor2a_0, cor2b_0
-    fnameA1, fnameB1, fnameL1 = lasco_1, cor2a_1, cor2b_1
-
+    #Cor2A, Cor2B, C2
+    fnameA0, fnameB0, fnameL0 = base_images  #cor2a_0, cor2b_0, lasco_0
+    fnameA1, fnameB1, fnameL1 = cme_images   #cor2a_1, cor2b_1, lasco_1
+    
     myfitsA0 = fits.open(fnameA0)
     ima0 = myfitsA0[0].data
     hdra0 = myfitsA0[0].header
@@ -44,14 +45,16 @@ if __name__ == "__main__":
     imL1 = myfitsL1[0].data
     hdrL1 = myfitsL1[0].header
 
-    headers = [hdrb1, hdrL1, hdra1]
-    ims = [np.transpose(imb1 - imb0), np.transpose((imL1 - imL0)), np.transpose(ima1 - ima0)]
-    
+    headers = [hdra1,hdrb1, hdrL1]
+    ims = [np.transpose(ima1 - ima0), np.transpose(imb1 - imb0), np.transpose((imL1 - imL0))]
     
     satpos, plotranges = pyGCS.processHeaders(headers)
-    sats = [["STEREO-A SECCHI", "COR2"], ["STEREO-B SECCHI", "COR2"], ["SOHO-LASCO", "C2"]]
-    
-    runGCSgui(ims, satpos, plotranges, sats, [5, 20, 30])
+    #breakpoint()
+    sats = [["STEREO-A", "COR2"], ["STEREO-B", "COR2"], ["LASCO", "C2"]]
+    date_obs = [hdra1['DATE-OBS'], hdrb1['DATE-OBS'], hdrL1['DATE-OBS']]
+    filenames = [hdra1['filename'], hdrb1['filename'], hdrL1['filename']]
+    #breakpoint()
+    runGCSgui(ims, satpos, plotranges, sats,date_obs, save_name, [5, 20, 30])
 
     # This is a test for running several events at once
     # start_time = parse_time("2011-06-05 07:00:00")
